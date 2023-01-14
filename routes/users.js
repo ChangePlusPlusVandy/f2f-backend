91d9a687eb32626528ff25a40a5a3c5a90976d58
Rana Dubauskas <ranadubauskas@gmail.com>
2023-01-14 16:20:28 -0600

    fix minor error

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,9 @@ const {getAllUsers, getUserById, addUser, deleteUser, updateUser} = require("../
 router.get('/', getAllUsers);
 router.get('/:id', getUserById);
 router.post('/', addUser);
-router.put('/:id', updateUser)
+router.delete('/:id', deleteUser);
+router.put('/:id', updateUser);
+
 
 
 module.exports = router;
