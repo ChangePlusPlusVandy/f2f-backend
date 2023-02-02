060b9e2980a6256d66afc40463a878f4d516317d
Rana Dubauskas <ranadubauskas@gmail.com>
2023-02-01 19:12:39 -0600

    changes

--- controllers/task-controller.js
+++ controllers/task-controller.js
@@ -34,10 +34,133 @@ const getTaskById = async (req, res) => {
     }
 }
 
+
+
+
+
+const getTaskByAttributes = async (req, res) => {
+    try {
+        const disability = req.query.disability;
+       // console.log(disability);
+        const age = req.query.age;
+       // console.log(age);
+        const time = req.query.time;
+       //console.log(time);
+        const allTasks = await Task.find();
+        if (disability && age && time){
+            let filteredTasks = [];
+            allTasks.forEach(task => {
+                let dis = task.disabilities;
+                for (let i = 0; i < dis.length; i++){
+                    if (dis[i] === disability){
+                        filteredTasks.push(task);
+                    }
+                }
+            });
+            //console.log(filteredTasks);
+            //console.log("here");
+            //console.log(filteredTasks);
+            let filteredByAgeAndTime = [];
+            for(let i = 0; i < filteredTasks.length; i++){
+                let curTask = filteredTasks[i];
+                console.log(curTask);
+                console.log(curTask.minAge);
+                if (age>=curTask.minAge && age<=curTask.maxAge && curTask.timePeriod==time){
+                    filteredByAgeAndTime.push(curTask);
+                }
+            }
+            return res.status(200).json(filteredByAgeAndTime);
+        }
+        else if (disability && age){
+            let filteredTasks = [];
+            allTasks.forEach(task => {
+                let dis = task.disabilities;
+                for (let i = 0; i < dis.length; i++){
+                    if (dis[i] === disability){
+                        filteredTasks.push(task);
+                    }
+                }
+            });
+            let filteredByAge = [];
+            for(let i = 0; i < filteredTasks.length; i++){
+                let curTask = filteredTasks[i];
+                if (age>=curTask.minAge && age<=curTask.maxAge){
+                    filteredByAge.push(curTask);
+                }
+            }
+            return res.status(200).json(filteredByAgeAndTime);
+        }
+        /*
+        else if (disability && time){
+            let filteredTasks = [];
+            allTasks.forEach(task => {
+                let dis = task.disabilities;
+                for (let i = 0; i < dis.length; i++){
+                    if (dis[i] === disability){
+                        filteredTasks.push(task);
+                    }
+                }
+            });
+            let filteredByTime = [];
+            for(let i = 0; i < filteredTasks.length; i++){
+                let curTask = filteredTasks[i];
+                if (curTask.timePeriod==time){
+                    filteredByTime.push(curTask);
+                }
+            }
+            return res.status(200).json(filteredByTime);
+        }
+                                */
+        else if (age && time){
+            let filteredByAgeAndTime = [];
+            for(let i = 0; i < allTasks.length; i++){
+                let curTask = filteredTasks[i];
+                if (age>=curTask.minAge && age<=curTask.maxAge && curTask.timePeriod==time){
+                    filteredByAgeAndTime.push(curTask);
+                }
+            }
+            return res.status(200).json(filteredByAgeAndTime);
+        }
+        else if (disability){
+            let disabilityTasks = [];
+            allTasks.forEach(task => {
+                let dis = task.disabilities;
+                for (let i = 0; i < dis.length; i++){
+                    if (dis[i] === disability){
+                        disabilityTasks.push(task);
+                    }
+                }
+            });
+            return res.status(200).json(disabilityTasks);
+        }
+        else if (age){
+            let filteredByAge = [];
+            for(let i = 0; i < allTasks.length; i++){
+                let curTask = filteredTasks[i];
+                if (age>=curTask.minAge && age<=curTask.maxAge){
+                    filteredByAgeAndTime.push(curTask);
+                }
+            }
+            return res.status(200).json(filteredByAgeAndTime);
+        }
+        else if (time){
+            let filteredByTime = allTasks.filter(task => )
+        }
+    }
+    catch(err){
+        console.log(err.message);
+        return res.status(500).send({message: err.message});
+    }
+}
+
+
+
+
+
 //get task by disability
 const getTaskByDisability = async (req, res) => {
     try{
-        const disability = req.body.disability;
+        const disability = req.query.disability;
         const allTasks = await Task.find();
         let disabilityTasks = [];
         allTasks.forEach(task => {
@@ -59,7 +182,7 @@ const getTaskByDisability = async (req, res) => {
 //get task by age:
 const getTaskByAge = async (req, res) => {
     try{
-        const age = req.body.age;
+        const age = req.query.age;
         const allTasks = await Task.find();
         let ageTasks = [];
         allTasks.forEach(task => {
@@ -80,8 +203,8 @@ const getTaskByAge = async (req, res) => {
 //get task by time:
 const getTaskByTime = async (req, res) => {
     try{
-        const timePeriod = req.body.timePeriod;
-        const allTasks = await Task.find({timePeriod: timePeriod});
+        const time = req.query.time;
+        const allTasks = await Task.find({timePeriod: time});
         return res.status(200).json(allTasks);
     }
     catch(err){
@@ -89,6 +212,7 @@ const getTaskByTime = async (req, res) => {
         return res.status(500).send({message: err.message});
     }
 }
+
 
 
 
@@ -145,4 +269,4 @@ const deleteTask = async (req, res) => {
 
 
 
-module.exports = {getAllTasks, getTaskById, getTaskByDisability, getTaskByAge, getTaskByTime, createTask, updateTask, deleteTask};
+module.exports = {getAllTasks, getTaskById, getTaskByAttributes, getTaskByDisability, getTaskByAge, getTaskByTime, createTask, updateTask, deleteTask};
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {getAllTasks, getTaskById, getTaskByAge, getTaskByDisability, getTaskByTime, createTask, updateTask, deleteTask} = require("../controllers/task-controller.js");
+const {getAllTasks, getTaskById, getTaskByAttributes, getTaskByAge, getTaskByDisability, getTaskByTime, createTask, updateTask, deleteTask} = require("../controllers/task-controller.js");
 
 router.get('/',  getAllTasks);
 router.get('/byId', getTaskById);
+router.get('/byAttributes', getTaskByAttributes);
 router.get('/byDisability', getTaskByDisability);
 router.get('/byAge', getTaskByAge);
 router.get('/byTime', getTaskByTime)
