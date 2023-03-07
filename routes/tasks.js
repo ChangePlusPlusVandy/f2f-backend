355a8bb4798b6d7930573ce5426928e61dd4f23d
Xiaohan Liu <xiaohan.liu.1@vanderbilt.edu>
2023-03-06 18:54:14 -0600

    finished task load csv method

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const {getAllTasks, getTaskById, getTaskByAttributes, getTaskByAge, getTaskByDisability, getTaskByTime, getTaskByPriority, createTask, updateTask, deleteTask} = require("../controllers/task-controller.js");
+const {
+  getAllTasks,
+  getTaskById,
+  getTaskByAttributes,
+  getTaskByAge,
+  getTaskByDisability,
+  getTaskByTime,
+  getTaskByPriority,
+  createTask,
+  updateTask,
+  deleteTask,
+  loadTaskCSV,
+} = require("../controllers/task-controller.js");
 
-router.get('/',  getAllTasks);
-router.get('/byPriority', getTaskByPriority);
-router.get('/byAge', getTaskByAge);
-router.get('/byAttributes', getTaskByAttributes);
-router.get('/byDisability', getTaskByDisability);
-router.get('/:id', getTaskById);
-router.get('/byTime', getTaskByTime);
-router.post('/', createTask);
-router.put('/:id', updateTask);
-router.delete('/:id', deleteTask);
-
-
+router.get("/", getAllTasks);
+router.get("/byPriority", getTaskByPriority);
+router.get("/byAge", getTaskByAge);
+router.get("/byAttributes", getTaskByAttributes);
+router.get("/byDisability", getTaskByDisability);
+router.get("/:id", getTaskById);
+router.get("/byTime", getTaskByTime);
+router.post("/", createTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
+router.post("/loadTaskCSV", loadTaskCSV);
 
 module.exports = router;
