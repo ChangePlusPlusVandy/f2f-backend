d7275087505853469babcddeaf484735f72228d3
Gabriel Dong <gabrielhdong@gmail.com>
2023-03-27 21:38:32 -0500

    updating

--- routes/verification.js
+++ routes/verification.js
@@ -4,7 +4,7 @@ const { checkMongo } = require("../controllers/verification-controller");
 const { checkSF } = require("../controllers/verification-controller");
 
 const router = express.Router();
-
+np
 router.get('/checkMongo', checkMongo);
 router.get('/sendEmail', sendVerifEmail);
 router.get('/checkSF', checkSF);
